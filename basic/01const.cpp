f7277fb08ad6a4fb6bc57213a334a3e324922e7c
Rubus <Rubus2550327>
2022-01-23 15:28:09 +0800

    Initial commit

---
+++ basic/01const.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Rubus on 2022/1/23.
+//
+#include "iostream"
+
+using namespace std;
+
+#define Day 7
+
+int main() {
+
+    const int month = 12;
+    cout << "one week has " << Day << " day" <<endl;
+    cout << "one year has " << month << " month" <<endl;
+    return 0;
+}
